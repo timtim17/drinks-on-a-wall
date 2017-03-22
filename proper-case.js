de4cac326e1bbd0939ed9dd5cad174d309193970
Austin Jenchi <ajenchi9@gmail.com>
2017-03-22 07:40:32 -0700

    Add link to source of proper-case.js
    Honestly it looks like 2014 me really didn't know how to code too well...

--- proper-case.js
+++ proper-case.js
@@ -1,3 +1,6 @@
+// looks like 2014 me didn't add a citation to where this code was from
+// it looks like it's from this Stack Overflow answer by Tuan
+// http://stackoverflow.com/a/5574446/1108513
 String.prototype.toProperCase = function () {
   return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
